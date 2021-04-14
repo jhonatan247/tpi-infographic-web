5a0a1e9d766cc85e05afc0fe0730a7f1cad24cf1
saduquebe <saduquebe@unal.edu.co>
2021-04-13 21:56:51 -0500

    antes del desastre

--- js/script.js
+++ js/script.js
@@ -1,16 +1,52 @@
 const modals = [
   {
-    title: 'Sistema TransMilenio Bogotá',
+    title: "Sistema TransMilenio Bogotá",
     body:
-      'El transporte en la ciudad de Bogotá ha sido sometido a análisis de corte financiero y técnico, que han concluido que, a pesar de ser visto en un principio como la solución a los problemas de movilidad de la ciudad, el sistema presenta varios problemas estructurales (alto precio de los pasajes, ineficacia para crecer con la demanda, bloqueos, protestas, largas filas, etc.) que desembocaron en una crisis multidimensional (financiera, técnica, sociocultural e incluso urbanística).',
+      "El transporte en la ciudad de Bogotá ha sido sometido a análisis de corte financiero y técnico, que han concluido que, a pesar de ser visto en un principio como la solución a los problemas de movilidad de la ciudad, el sistema presenta varios problemas estructurales (alto precio de los pasajes, ineficacia para crecer con la demanda, bloqueos, protestas, largas filas, etc.) que desembocaron en una crisis multidimensional (financiera, técnica, sociocultural e incluso urbanística).",
   },
   {
-    title: 'Sistema SIT Curitiba, Brasil',
+    title: "Transporte de última milla en Seúl",
     body: `
     <div class="row">
       <div class="col-6">
         <p>
-          El éxito del sistema BRT de Curitiba se puede atribuir a distintos factores. Uno de ellos es que el 75% del área metropolitana de Curitiba está cubierta por el sistema BRT. Expertos indican que: “Habiendo varios medios de transporte accesibles, el viajero discapacitado tiene varias opciones; por ejemplo, una persona en silla de ruedas puede llamar a un taxi, ir al paradero a tomar un autobús Ligeirinho y transbordar a otro Ligeirinho en una terminal de integración para llegar a su destino.”
+        Un estudio realizado en Seúl mostró factores de la popularidad que han ganado los modelos de scooters eléctricos compartidos en la ciudad. Comparan además, factores de tiempo de desplazamiento, tiempo de acceso y costo con medios de transporte más convencionales, como buses de última milla o incluso los desplazamientos a pie.
+        </p>
+        <div class="plus-button" onclick="showInnerModal(0)">+</div>
+      </div>
+      <div class="col-6">
+        <img
+          class="w-100"
+          src="media/busSeul.jpg"/>
+      </div>
+    </div>
+    `,
+  },
+  {
+    title: "Bike-Sharing en Portland",
+    body: `
+    <div class="row">
+      <div class="col-6">
+        <p>
+        En Portland, Oregon, First Transit llegó a un acuerdo con Lyft para BIKETOWN, un sistema de bicicletas eléctricas compartidas. Este sistema permite la operación de 1500 bicicletas a lo largo de 30 millas cuadradas, con aproximadamente 140 estaciones en total. Este modelo, en menor o mayor medida, lo impulsan ciudades como Kansas City, San Francisco o Dayton en Estados Unidos, y múltiples ciudades más a nivel mundial.
+        </p>
+        <div class="plus-button" onclick="showInnerModal(0)">+</div>
+      </div>
+      <div class="col-6">
+        <img
+          class="w-100"
+          src="media/largeBiketown.jpg"/>
+      </div>
+    </div>
+    `,
+  },
+  {
+    title: "Efectos a la salud en Europa",
+    body: `
+    <div class="row">
+      <div class="col-6">
+        <p>
+        El uso de alternativas de última milla en Europa ha impactado positivamente a la salud de los habitantes. Especialmente los desplazamientos en bicicletas (incluso eléctricas) tienen impactos positivos en la salud, ya que aumentan la cantidad de actividad física comparada con los no-usuarios. Además, estos modelos de transporte incentivan la reducción del uso de automóviles. Sin embargo, la reducción ha sido marginal en el corto plazo.
         </p>
         <div class="plus-button" onclick="showInnerModal(0)">+</div>
       </div>
@@ -23,12 +59,12 @@ const modals = [
     `,
   },
   {
-    title: 'Transporte masivo en el mundo',
+    title: "Modelos de transporte a futuro",
     body: `
     <div class="row">
       <div class="col-6">
         <p>
-          Ciudades en todo el mundo ven como un complemento a los sistemas BRT la implementación de un sistema Metro, especialmente cuando la demanda supera los picos de 40.000 pasajeros hora/sentido. “Sin embargo, optar por un Tren a Nivel (Tren Ligero o Tranvía) o simples Carriles de Buses es anacrónico."
+        Estudios indican que, en comparación a un servicio con rutas y horarios fijos, un sistema de última milla emergente opera en una amplia diversidad de contextos. Diversos experimentos computacionales muestran que se mejoran los tiempos de espera de los pasajeros, de igual manera que sus tiempos de desplazamiento. Esto se logra haciendo uso de información del transporte masivo como hora de llegada de los trenes o buses.
         </p>
         <div class="plus-button" onclick="showInnerModal(0)">+</div>
       </div>
@@ -41,25 +77,7 @@ const modals = [
     `,
   },
   {
-    title: 'Alternativas de transporte en el mundo',
-    body: `
-    <div class="row">
-      <div class="col-6">
-        <p>
-        La ciudad pionera en el transporte compartido en el mundo fue Amsterdam, que en 1965 inauguró el primer modelo de transporte público de bicicletas compartidas. Desde ese momento, se ha iterado en este modelo hasta hoy, donde ciudades como Munich, París, Barcelona y Ciudad de México poseen modelos de bicicletas públicas compartidas que han desembocado en un mayor uso de vías públicas, disminución de emisión de gases de invernadero, y una mejora en salud pública.
-        </p>
-        <div class="plus-button" onclick="showInnerModal(0)">+</div>
-      </div>
-      <div class="col-6">
-        <img
-          class="w-100"
-          src="https://www.flaticon.es/premium-icon/icons/svg/773/773869.svg"/>
-      </div>
-    </div>
-    `,
-  },
-  {
-    title: 'Ineficiencia del transporte público Bogotano',
+    title: "Ineficiencia del transporte público Bogotano",
     body: `
     <div class="row">
       <div class="col-6">
@@ -77,7 +95,7 @@ const modals = [
     `,
   },
   {
-    title: 'Baja utilización de alternativas complementarias con el SITP',
+    title: "Baja utilización de alternativas complementarias con el SITP",
     body: `
     <div class="row">
       <div class="col-6">
@@ -95,7 +113,7 @@ const modals = [
     `,
   },
   {
-    title: 'Se requieren plantear soluciones al desplazamiento de última milla',
+    title: "Se requieren plantear soluciones al desplazamiento de última milla",
     body: `
     <div class="row">
           <div class="col-6">
@@ -117,64 +135,72 @@ const modals = [
     `,
   },
   {
-    title: 'Política',
+    title: "Política",
     body: `
     <div class="row">
       <div class="col-6">
         <p>
-          Entre los diferentes proyectos de inversión propuestos se encuentra la expansión de la oferta de SITP-transmilenio (proyecto 7782 del PDD 2020-2024), la construcción del metro de la ciudad como alternativa del transporte masivo (proyecto 7501 del PDD 2020-2024), la articulación de diferentes medios de transporte (proyecto 7786 del PDD 2020-2024), y el fomento de apropiación ciudadana del Sistema Integrado de Transporte Público de Bogotá (proyecto 7513_262 del PDD 2020-2024).
+        En materia de política pública la movilidad es uno de los principales puntos de interés de cualquier administración de la ciudad, para cuyo mejoramiento deben articularse tanto políticas previas como actuales de acuerdo con los principios adoptados según iniciativas como la iniciativa Visión Cero, y los objetivos establecidos en planes organizacionales de largo plazo como lo son el Plan Maestro de Movilidad y el Plan Distrital de Desarrollo.
         </p>
         <div class="plus-button" onclick="showInnerModal(0)">+</div>
       </div>
       <div class="col-6">
         <img
           class="w-100"
-          src="https://www.flaticon.es/premium-icon/icons/svg/773/773869.svg"/>
+          src="media/VisionCerologo.png"/>
+          <img class="w-100" src="media/politica.PNG"/>
       </div>
     </div>
     `,
   },
   {
-    title: 'Economía',
+    title: "Economía",
     body: `
     <div class="row">
       <div class="col-6">
         <p>
-        En el aspecto económico, para el año 2021, la ciudad tiene planeada una inversión de $3.180.250.700.000 en lo que respecta al sector de movilidad, 21,1% del presupuesto total. Dicho monto está repartido en $430.877.239.000 para la Secretaría Distrital de Movilidad, $2.615.039.772.000 para el Instituto de Desarrollo Urbano y $134.333.689.000 para la Unidad Administrativa Especial de Rehabilitación y Mantenimiento Vial, de acuerdo con lo establecido en el Plan Operativo Anual de Inversiones (POAI) 2021 (SDP, 23 de diciembre de 2020). <br/> En el caso de la inversión enfocada directamente en transporte masivo, para 2021, se cuenta con un presupuesto aproximado de $400 mil millones, de los cuales $6.421 millones de pesos corresponden con la inversión en los objetivos 3 y 4 del proyecto de Fortalecimiento de una movilidad sostenible y accesible para Bogotá y su Región (DNP, 15 de julio de 2020), a cargo de la SDM; $210.620.767.000 corresponden con la inversión en el proyecto de  Infraestructura para el Sistema Integrado de Transporte Público Sostenible, y $183.500.000.000 corresponden con el proyecto de Integración funcional del Regiotram a la estructura urbana de la Ciudad, ambos a cargo del IDU (SDP, 23 de diciembre de 2020).
+        En el aspecto económico, para el año 2021, la ciudad tiene planeada una inversión de $3.180.250.700.000 en lo que respecta al sector de movilidad, 21,1% del presupuesto total. Dicho monto está repartido en $430.877.239.000 para la Secretaría Distrital de Movilidad, $2.615.039.772.000 para el Instituto de Desarrollo Urbano y $134.333.689.000 para la Unidad Administrativa Especial de Rehabilitación y Mantenimiento Vial, de acuerdo con lo establecido en el Plan Operativo Anual de Inversiones (POAI) 2021 (SDP, 23 de diciembre de 2020).
         </p>
         <div class="plus-button" onclick="showInnerModal(0)">+</div>
       </div>
       <div class="col-6">
         <img
           class="w-100"
-          src="https://www.flaticon.es/premium-icon/icons/svg/773/773869.svg"/>
+          src="media/econo.png"/>
+          
       </div>
     </div>
     `,
   },
   {
-    title: 'Social',
+    title: "Social",
     body: `
     <div class="row">
       <div class="col-6">
         <p>
-          La percepción de los habitantes sobre la mayoría de los medios de transporte disponibles en la ciudad es algo baja respecto a lo deseado, puesto que solo el Cable tiene una puntuación superior a 4 en una escala sobre 5 para la calificación de la experiencia de viaje. Le siguen la bicicleta con una puntuación de 3,9, el desplazamiento a pie, la patineta, la moto y el transporte informal con una puntuación de 3,8, mientras que el taxi y el auto tienen 3,7 puntos, el transporte escolar y otros medios de transporte tienen 3,6 puntos y el transporte intermunicipal tiene 3,4, los alimentadores y SITPs provisionales tienen 3,3, y el bicitaxi tiene 3,2 puntos.
+        En relación con el uso de medios de transporte diferentes a los medios motorizados convencionales para viajes intermodales es posible evidenciar que existe una adopción mínima de medios como las patinetas y bicitaxis y otros medios de transporte, los cuales en conjunto corresponden a sólo el 1,5% de las etapas de viaje de los ciudadanos, de acuerdo con los resultados de la encuesta de movilidad 2019.
         </p>
         <div class="plus-button" onclick="showInnerModal(0)">+</div>
       </div>
       
       <div class="col-6">
-        <img class="w-100" src="./media/social1.png"/>
+        <img class="w-100" src="media/barrasSocial.png"/>
+        
       </div>`,
   },
   {
-    title: 'Tecnología',
+    title: "Tecnología",
     body: `
     <div class="row">
       <div class="col-6">
         <p>
-          En relación con el transporte público hay información más detallada de acuerdo con las cantidades y tecnologías de su flota. De acuerdo con lo presentado por Transmilenio S.A en su informe de estadísticas de oferta y demanda a febrero de 2021, la flota se compone de 2357 buses de servicio troncal, 931 buses alimentadores y 6174 buses de servicio zonal. Según el nivel de emisiones, el primer grupo está conformado en un 10% por buses Euro II, III y IV, en un 20% por buses Euro V, en un 29% por buses Euro V con filtro, en un 31% por buses Euro VI y en un 10% por buses híbridos. En el caso de los alimentadores el 13% corresponde a buses Euro II, III y IV, el 54% a Euro V, el 14% a Euro VI, el 5% a híbridos y el 14% a buses eléctricos. Y en el caso de los buses zonales, el 24% corresponde a buses Euro I, II y III, el 16% a buses Euro IV, el 46% a buses Euro V, el 1% a buses híbridos, el 1% a Euro VI, el 10% a Euro VI-GNC y el 2% a buses eléctricos.
-        </p>
+        En relación con el transporte público hay información detallada sobre el tamaño de su flota y la tecnología utilizada. De acuerdo con lo presentado por Transmilenio S.A en su informe de estadísticas de oferta y demanda a febrero de 2021, la flota se compone de:
+          </p>
+        <ul>
+        <li>2357 buses de servicio troncal</li>
+        <li>931 buses alimentadores</li>
+        <li>6174 buses de servicio zonal.</li>
+        </ul>
         <div class="plus-button" onclick="showInnerModal(0)">+</div>
       </div>
       
@@ -183,7 +209,7 @@ const modals = [
       </div>`,
   },
   {
-    title: 'Ambiental',
+    title: "Ambiental",
     body: `
     <div class="row">
       <div class="col-6">
@@ -191,20 +217,20 @@ const modals = [
           <li>El sector transporte en Bogotá, representa el 58% de las emisiones de GEI en la Ciudad, lo que se traduce en una emisión estimada de  0.6 toneladas de CO2eq por año por parte de los bogotanos (DNP, 2020).</li>
           <li>Para el 31 de diciembre de 2019, se habían matriculado en Bogotá 1.466 vehículos exclusivamente eléctricos, siendo tan solo el 0.06% del total de vehículos registrados en la ciudad (DNP, 2020).</li>
           <li>Existen diferentes normas en materia ambiental que promueven la adopción de tecnologías de energía renovable y estrategias de eficiencia energética, como la ley 1715 de 2014.</li>
-          <li>Departamento Nacional de Planeación (DNP). (15 de julio de 2020). Implementación del sistema de  transporte de cero y bajas emisiones para Bogotá. Secretaría Distrital de Movilidad. Recuperado el 20 de marzo de 2021 de <a target="blank" href="http://www.sdp.gov.co/sites/default/files/113_sdm_2020110010120_7583_0.pdf">sdp.gov.co</a></li>
+          <!--<li>Departamento Nacional de Planeación (DNP). (15 de julio de 2020). Implementación del sistema de  transporte de cero y bajas emisiones para Bogotá. Secretaría Distrital de Movilidad. Recuperado el 20 de marzo de 2021 de <a target="blank" href="http://www.sdp.gov.co/sites/default/files/113_sdm_2020110010120_7583_0.pdf">sdp.gov.co</a></li>-->
         </ul>
         <div class="plus-button" onclick="showInnerModal(0)">+</div>
       </div>
       <div class="col-6">
         <img
           class="w-100"
-          src="https://www.flaticon.es/premium-icon/icons/svg/773/773869.svg"/>
+          src="media/ambiental.png"/>
       </div>
     </div>
     `,
   },
   {
-    title: 'Legal',
+    title: "Legal",
     body: `
     <div class="row">
       <div class="col-6">
@@ -212,9 +238,9 @@ const modals = [
           <li>A nivel nacional todos los actores viales se encuentran sujetos a lo establecido en el Código Nacional de Tránsito.</li>
           <li>El artículo 164 del Decreto 190 de 2004, del Plan de Ordenamiento Territorial establece los componentes del subsistema vial de Bogotá.</li>
           <li>La Secretaría de Movilidad de Bogotá cumple el papel de autoridad de tránsito y transporte (decreto 672 de 2018), incluyendo funciones de control e investigación en materia de transporte público.</li>
-          <li>El decreto 1079 de 2015 o decreto único reglamentario del sector transporte, que dicta las disposiciones generales para el préstamo del servicio de transporte en las diferentes modalidades (servicio público de transporte colectivo, SETP, transporte público masivo, taxis, servicio de transporte mixto, en motocarro, servicio de transporte especial).</li>
+          <!--<li>El decreto 1079 de 2015 o decreto único reglamentario del sector transporte, que dicta las disposiciones generales para el préstamo del servicio de transporte en las diferentes modalidades (servicio público de transporte colectivo, SETP, transporte público masivo, taxis, servicio de transporte mixto, en motocarro, servicio de transporte especial).</li>
           <li>La resolución 3256 de 2018 del Ministerio de Transporte, reglamenta y autoriza la prestación del servicio público de transporte de pasajeros en triciclos o tricimóviles no motorizados y tricimóviles con pedaleo asistido.</li>
-          <li>La resolución 336 de 2019 de la Secretaría de Movilidad, la cual regula la autorización para el préstamo de patinetas en espacios públicos.</li>
+          <li>La resolución 336 de 2019 de la Secretaría de Movilidad, la cual regula la autorización para el préstamo de patinetas en espacios públicos.</li>-->
         </ul>
         <div class="plus-button" onclick="showInnerModal(0)">+</div>
       </div>
@@ -229,12 +255,12 @@ const modals = [
 ];
 
 window.showModal = function (index) {
-  $('#infoModalLabel').html(modals[index].title);
-  $('#infoModalBody').html(modals[index].body);
-  $('#infoModal').modal('show');
+  $("#infoModalLabel").html(modals[index].title);
+  $("#infoModalBody").html(modals[index].body);
+  $("#infoModal").modal("show");
 };
 
 window.showInnerModal = function (index) {
-  $('#infoModalLabel').html(modals[index].title);
-  $('#infoModalBody').html(modals[index].body);
+  $("#infoModalLabel").html(modals[index].title);
+  $("#infoModalBody").html(modals[index].body);
 };
